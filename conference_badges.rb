457d8e3f62eebf641b00d1c092b2fe395659919f
sami suliman <sami.bco@gmail.com>
2019-08-17 20:21:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,7 @@
 # Write your code here.
 def onference_badges(neme)
   name = [Edsger, Ada, Charles, Alan, Grace, Linus,  Matz]
-  name.each do ||
+  name.each do |attendees|
+    puts 
+  end
 end 
